ed2436d813e4a61cb914ebf1bc162bf980608129
basmaaitlechgueur-blip <basmaaitlechgueur@gmail.com>
2026-03-14 15:09:52 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Test Tailwind
+      </h1>
+      <p className="mt-4 text-gray-600">
+        Si ce texte est en bleu, Tailwind fonctionne !
+      </p>
+    </div>
+  )
+}
+
+export default App
