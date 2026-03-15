1fef1807dc3cb7fe9d234bc81dc2e27318381f3e
basmaaitlechgueur-blip <basmaaitlechgueur@gmail.com>
2026-03-15 11:56:16 +0100

    J'ai ajouté un nouveau projet

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -28,7 +28,7 @@ export const certifications: Certification[] = [
     title: "Développement Front-End moderne avec React",
     issuer: "MLIAEdu",
     issueDate: "2026-03",
-    credentialUrl: "https://coursera.org/verify/example",
+    credentialUrl: "https://mliaedu.toubkalit.com/verify-certificate/26-cd90af6b-8e94-4e1a-9805-92526f204bb6-819603",
     skills: ["React"],
     tags: ["Frontend", "React"],
     status: "active"
